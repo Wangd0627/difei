0329988e3533719b54d17696312cd6e5b0c0ab93
Wangd0627 <difei.wang@umszki.hu>
2026-09-24 10:19:51 +0200

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,23 @@
+print ("hello word")
+print (2026, ".", "09",".",24,".")
+Wang=input("hogy hívnak?")
+print("szia", Wang )
+
+
+évszám=int(input("Melyik évben születtél?"))
+print(type(évszám))
+print(type(2026))
+kor= 2026-évszám
+print("Te", kor, "éves vagy")
+
+
+
+
+
+
+
+
+
+
+
+
